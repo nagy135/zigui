b17d24ba379b24a5b1a869e8d2bcfec194ebb489
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-11-13 11:10:49 +0100

    feat: basic test with loop

--- src/main.zig
+++ src/main.zig
@@ -6,12 +6,14 @@ pub fn main() anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screenWidth, screenHeight, "Hello");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
+    const poc = rl.Vector3.init(200, 200, 1);
+    var shift = rl.Vector3.init(0, 0, 0);
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
@@ -19,6 +21,12 @@ pub fn main() anyerror!void {
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
+        rl.drawCube(poc.add(shift), 109, 109, 2, rl.Color.red);
+        rl.drawCube(poc.subtract(shift), 109, 109, 2, rl.Color.red);
+        shift.x += 0.5;
+        shift.y += 0.2;
+
+        //----------------------------------------------------------------------------------
         // Draw
         //----------------------------------------------------------------------------------
         rl.beginDrawing();
@@ -26,7 +34,7 @@ pub fn main() anyerror!void {
 
         rl.clearBackground(rl.Color.white);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        rl.drawText("Whats up", 190, 200, 20, rl.Color.gold);
         //----------------------------------------------------------------------------------
     }
 }
